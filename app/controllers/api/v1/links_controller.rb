9509bdb54307c85ee4192bfadfde55913db04963
sjBao <supuilam@gmail.com>
2017-05-10 13:48:49 -0400

    Add Links#show and Links#create in controller

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -2,4 +2,24 @@ class Api::V1::LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
+  def show
+    @link = Link.find_by(params[:id]) || Link.find_by(params[:url])
+    render :show if !!@link
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      render :show
+    else
+      render :error
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
+  end
 end
