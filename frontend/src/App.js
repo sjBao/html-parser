ca1420a443c5c116eace9eb660c3f2cc1f1e06ef
sjBao <supuilam@gmail.com>
2017-05-11 13:58:00 -0400

    Prototype nav-bar with react-router.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const History = () => (
+  <div>
+    <h2>History</h2>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/history">History</Link>
+        </li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home} />
+      <Route exact path="/history" component={History} />
+    </div>
+  </Router>
+)
 
 export default App;
